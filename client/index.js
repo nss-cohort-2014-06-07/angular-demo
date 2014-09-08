231dd0f800dcb124deca383cec3ec55466f8118c
Chyld Medford <chyld.medford@gmail.com>
2014-09-08 14:00:07 -0500

    done

--- client/index.js
+++ client/index.js
@@ -4,6 +4,12 @@
   angular.module('demo', [])
   .controller('MainController', ['$scope', function($scope){
     $scope.name = 'Groot!';
+    $scope.age = 5;
+    $scope.dogs = [
+      {name:'fido',   age:3},
+      {name:'baxter', age:5},
+      {name:'fluffy', age:7}
+    ];
   }]);
 })();
 
